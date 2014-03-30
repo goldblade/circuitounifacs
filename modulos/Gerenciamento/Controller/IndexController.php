a8b0826dc20f1dc1114c44e9d3813b7eb1ce8acd
Eduardo Junior <eduardojunior@eduardojunior.com>
2014-03-30 16:54:54 -0300

    gerenciamento add

---
+++ modulos/Gerenciamento/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+namespace modulos\Gerenciamento\Controller;
+
+use Unifacs\Core\Controller\ActionController;
+
+class IndexController extends ActionController
+{
+
+	public function indexAction()
+	{			
+		return self::renderHtml(array(
+		
+		));
+	}
+	
+}
