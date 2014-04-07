d16f0f4f910a56e7152d78bb348fbacc97a1642b
Bonifácio Júnior <boni.g.jr@gmail.com>
2014-04-06 22:56:38 -0300

    Novas views

---
+++ modulos/Aplicacao/View/eventos/inscricoes.php
@@ -0,0 +1,44 @@
+<div class="container">
+	
+	<ol class="breadcrumb">
+  		<li><a href="/aplicacao/index/index/usuario/2">Ínicio</a></li>
+  		<li><a href="/aplicacao/eventos/index/usuario/2">Eventos</a></li>
+  		<li class="active">Minhas Inscrições</li>
+	</ol>
+
+	<div class="page-header">
+		<h1>Minhas Inscrições</h1>
+	</div>	
+	<div class="row-fluid">
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th class="col-sm-9">Evento</th>
+					<th class="col-sm-3">Ações</th>
+				</tr>
+				
+			</thead>
+			<tbody>
+				<tr>
+					<td>Palestra Android</td>
+					<td class="text-center">
+						<a href="#" class="btn btn-info"><i class="glyphicon glyphicon-print"></i> Certificado</a>
+						<a href="#" class="btn btn-info"><i class="glyphicon glyphicon-list-alt"></i> Detalhes</a>
+						<a href="#" class="btn btn-danger"><i class="glyphicon glyphicon-trash"></i> Cancelar</a>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="row-fluid">
+		<div class="col-sm-12 text-center">
+			<ul class="pagination ">
+			  	<li><a href="#">&laquo;</a></li>
+			  	<li><a href="#">1</a></li>
+			  	<li><a href="#">2</a></li>
+			  	<li><a href="#">3</a></li>
+				<li><a href="#">&raquo;</a></li>
+			</ul>
+		</div>		
+	</div>
+</div>	
