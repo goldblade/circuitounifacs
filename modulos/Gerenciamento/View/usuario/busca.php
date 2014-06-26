cd693d0a643c35a41d37cab8d883d624dd8744f2
Eduardo Junior <ej@eduardojunior.com>
2014-06-25 21:22:22 -0300

    busca

--- modulos/Gerenciamento/View/usuario/busca.php
+++ modulos/Gerenciamento/View/usuario/busca.php
@@ -1,25 +1,26 @@
 <?php
-$i = 0;
 foreach ($dados as $key => $dado):
+	foreach ($dado as $key2 => $value2):
 ?>
 	<tr>
 		<td>
 			<?php
 			// var_dump($dado[0]['nome']);
-			echo $dado[$i]['nome'];
+			// echo $dado[$i]['nome'];
+			echo $value2['nome'];
 			?>
 		</td>
 		<td class="text-center">						
 			<a href="/gerenciamento/usuario/save/id/<?php
-			echo $dado[$i]['id'];
+			echo $value2['id'];
 			?>" 
 			class="btn btn-info"><i class="glyphicon glyphicon-pencil"></i> Editar</a>
 			<a href="/gerenciamento/usuario/apagar/id/<?php 
-			echo $dado[$i]['id'];?>" 
+			echo $value2['id'];?>" 
 			class="btn btn-danger btnapagar"><i class="glyphicon glyphicon-trash"></i> Apagar</a>
 		</td>
 	</tr>
 <?php
-	$i++;
+	endforeach;
 endforeach;
 ?>
